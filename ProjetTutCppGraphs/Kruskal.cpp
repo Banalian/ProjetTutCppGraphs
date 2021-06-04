174cb2d1a33df0f6daeee8c62e1fce632fd889d3
CorentinEsn <75800644+CorentinEsn@users.noreply.github.com>
2021-06-04 16:10:11 +0200

    Created Ensemble struct, continued Algorithm

--- ProjetTutCppGraphs/Kruskal.cpp
+++ ProjetTutCppGraphs/Kruskal.cpp
@@ -1,5 +1,11 @@
 #include "Cgraphe.h"
 
+typedef struct SensembleSommets
+	{
+		Csommet** pSOMtab;
+		int iTailleTab;
+	};
+
 /**
  * @brief Permet de chercher un arbre couvrant de poids minimal a aprtir d'un graphe
  * 
@@ -11,17 +17,22 @@ Cgraphe* kruskal(Cgraphe* pGRAbase) {
 	int iBoucle;
 	int iNbSomTotal = pGRAbase->GRAGetNbSommets();
 	int iNbArcsTotal = pGRAbase->GRAgetNbAllArcs();
+	
 	Csommet** pSOMtabSommetsGraphe = pGRAbase->GRAgetTabSomCopy();
 	Carc** pARCtabArcsTotalGraphe = pGRAbase->GRAgetTabAllArcsCopy();
 	
 	Cgraphe* pGRAresult = new Cgraphe();
-	
+	SensembleSommets** pENSTabEnsemble = new SensembleSommets*[iNbSomTotal];
 
 	for (iBoucle = 0; iBoucle < iNbSomTotal; iBoucle++) {
 		//on ajoute tt les sommets au graphe
 		pGRAresult->GRAAddSommet(pSOMtabSommetsGraphe[iBoucle]->SOMGetSomNum());
 
 		//l'ensemnble Qk contient Sk
+		pENSTabEnsemble[iBoucle] = new SensembleSommets();
+		pENSTabEnsemble[iBoucle]->iTailleTab = 1;
+		pENSTabEnsemble[iBoucle]->pSOMtab = new Csommet*[1];
+		pENSTabEnsemble[iBoucle]->pSOMtab[0] = new Csommet(pSOMtabSommetsGraphe[iBoucle]->SOMGetSomNum());
 	}
 
 	//trier tab arc ordre croissant du poids
@@ -29,11 +40,16 @@ Cgraphe* kruskal(Cgraphe* pGRAbase) {
 	for (iBoucle = 0; iBoucle < iNbArcsTotal; iBoucle++) {
 		//soit (u,v) l'arrete pARCtabArcsTotalGraphe[iBoucle]
 		//trouver les 2 ensmbles qui ont le sommet u et le sommet v
+		do {
+			//rechercher dans les ensembles chaque som
+		} while (pARCtabArcsTotalGraphe[iBoucle]->ARCgetDepart() != pENSTabEnsemble[0]->pSOMtab[0]->SOMGetSomNum());
 		//Si Gu != Gv
+		if (pENSTabEnsemble[iBoucle] != pENSTabEnsemble[iBoucle]) {
 			pGRAresult->GRAAddArc(0, 0);//add arc u,v
 			//fusionner Qv et Qu
-		//fin si
+		}
 	}
 
 	return pGRAresult;
 }
+
